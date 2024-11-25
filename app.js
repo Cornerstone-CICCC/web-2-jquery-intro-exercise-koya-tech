396688067afbfacd5ea1c87dbf50ae6bfbf2a3ef
koya <koya9431ptr@gmail.com>
2024-11-25 19:07:09 +0000

    done

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
+  $(document).ready(function () {
+    $('#toggleButton').click(function () {
+      $('#myParagraph').toggle();
+    });
+  });
+  $('#colorButton').click(function () {
+    $('#colorDiv').css('background-color', 'yellow');
+  });
+
+  $('#addClassButton').click(function () {
+    $('#classDiv').addClass('newClass');
+  });
+
+  $('#removeClassButton').click(function () {
+    $('#classDiv').removeClass('newClass');
+  });
+
+  $('#fadeInButton').click(function () {
+    $('#fadeDiv').fadeIn();
+  });
+
+  $('#fadeOutButton').click(function () {
+    $('#fadeDiv').fadeOut();
+  });
+
+  $('#slideUpButton').click(function () {
+    $('#slideDiv').slideUp();
+  });
+
+  $('#slideDownButton').click(function () {
+    $('#slideDiv').slideDown();
+  });
 })
